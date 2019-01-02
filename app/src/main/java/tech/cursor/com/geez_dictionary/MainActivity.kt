022c10b1c77173a3ee27114e593cde7ffc3bbbad
Yimesgen Morka <webDesigner299@gmail.com>
2019-01-02 15:47:34 +0300

    adding Note inside MainActivity.kt for contributors

--- app/src/main/java/tech/cursor/com/geez_dictionary/MainActivity.kt
+++ app/src/main/java/tech/cursor/com/geez_dictionary/MainActivity.kt
@@ -5,8 +5,20 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Note
+     * Please use kotlin Language code for uniformity, but if you are not aware of kotlin Language
+     * You man use built in java to kotlin converter inside you android studio if you are using android studio as you primary development environment
+     * to convert any java code to kotlin equivalent please follow the following steps
+     * 1. open Your android studio program
+     * 2. after opening, select Code tab inside android studio
+     * 3. after selecting, find the option 'Convert Java File to Kotlin File' and select it!
+     *
+     * Android Studio will do the rest for You!
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
