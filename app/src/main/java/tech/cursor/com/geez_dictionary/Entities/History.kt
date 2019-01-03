631c5fab6ace56739ddfdc4bfe97815851b17bf1
Yimesgen Morka <webDesigner299@gmail.com>
2019-01-03 13:51:43 +0300

    Changes 1. add Two class (inside .Entities package i.e Word.kt and History.kt 2. Migrate to androidx ... migrating to  androidx (ask Google if you don not know about migration to androidx package) 3. add Room Libraries, room runtime and room compiler in build.gradle. 4. Removed material Package from gradle file.

---
+++ app/src/main/java/tech/cursor/com/geez_dictionary/Entities/History.kt
@@ -0,0 +1,7 @@
+package tech.cursor.com.geez_dictionary.Entities
+
+/**
+ * id: Word Id
+ * word: Actual word
+ */
+data class History(val id: Int,val word: String)
--- app/src/main/java/tech/cursor/com/geez_dictionary/MainActivity.kt
+++ app/src/main/java/tech/cursor/com/geez_dictionary/MainActivity.kt
@@ -1,6 +1,6 @@
 package tech.cursor.com.geez_dictionary
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
